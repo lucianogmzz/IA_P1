a7ce0e1bc42cff6b199dfb2ade36b79b88c21a54
lucianogmzz <A22310214@live.ceti.mx>
2025-03-06 20:32:54 -0600

    Practica 2

--- 002_Caracteres_de_escape.py
+++ 002_Caracteres_de_escape.py
@@ -1,3 +1,3 @@
-print("Hola, mi nombre es Luciano, aquí unos datos sobre mi...")
-print()
-print("-Me gusta ir a conciertos, mi banda favorita es Shame.\n-Tengo 21 años.\n-Me gustan los tacos de tripa.\n-La proxima semana veré a mi banda favorita de pequeño. ")
+print("Hola, mi nombre es Luciano, aquí unos datos sobre mi...")  #print de titulo, sin saltos de linea.
+print()  #print para testear una funcion vacía
+print("-Me gusta ir a conciertos, mi banda favorita es Shame.\n-Tengo 21 años.\n-Me gustan los tacos de tripa.\n-La proxima semana veré a mi banda favorita de pequeño. ") #print para enlistar cadenas, utilizando \n (newline)
