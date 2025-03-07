d042677ca8a04c899d2cc28f00c91540a71a629b
lucianogmzz <A22310214@live.ceti.mx>
2025-03-06 20:28:33 -0600

    Practica 2

---
+++ 002_Caracteres_de_escape.py
@@ -0,0 +1,3 @@
+print("Hola, mi nombre es Luciano, aquí unos datos sobre mi...")
+print()
+print("-Me gusta ir a conciertos, mi banda favorita es Shame.\n-Tengo 21 años.\n-Me gustan los tacos de tripa.\n-La proxima semana veré a mi banda favorita de pequeño. ")
