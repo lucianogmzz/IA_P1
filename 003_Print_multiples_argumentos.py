654cf7542ab1a604008ba32511dd4322c4b97e13
lucianogmzz <A22310214@live.ceti.mx>
2025-03-06 20:39:12 -0600

    Practica 3

---
+++ 003_Print_multiples_argumentos.py
@@ -0,0 +1,2 @@
+print("Testeo para print de multiples argumentos")  # Cadena completa
+print("Testeo para","print de","multiples argumentos")  # Print con varios argumentos, misma cadena, separada por comas
