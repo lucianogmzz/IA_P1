13ec1f5ae67a7e45cfd3b9eaa46d95ea38cbf620
lucianogmzz <A22310214@live.ceti.mx>
2025-03-09 15:31:51 -0600

    Practica 5

---
+++ 005_Variables.py
@@ -0,0 +1,13 @@
+#declaracion de variables 
+john = 3
+mary = 5
+adam = 6
+total_manzanas = john + mary + adam #nueva variable para la summa de las otras
+
+#utilizando la propiedad print para impirmir las variables
+print("-john tiene", john, "manzanas") 
+print("-mary tiene", mary, "manzanas")
+print("-adam tiene", adam, "manzanas\n")
+
+print("en conjunto tienen", total_manzanas, "manzanas")
+
