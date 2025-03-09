e0b0bd2dca38ab94bfdcc3164c68e75ff3bad857
lucianogmzz <A22310214@live.ceti.mx>
2025-03-09 15:44:18 -0600

    Practica 6

---
+++ 006_Operadores_y_variables.py
@@ -0,0 +1,17 @@
+#evaluando expresiones mediante operadores.
+
+x = 0
+x = float(x)
+y = 2 * x**3 + 5 * x**2 - 10 * x - 20 #python utiliza la jerarquia de operaciones convencional.
+print("el valor de y evaluada en 0 es\ny=", y)
+
+x = 2 #el valor de una variable puede reescribirse, por lo que no es necesario hacer muchas variables
+x = float(x)
+y = 2 * x**3 + 5 * x**2 - 10 * x - 20
+print("\nel valor de y evaluada en 2 es\ny=", y)
+
+x = 3
+x = float(x)
+y = 2 * x**3 + 5 * x**2 - 10 * x - 20
+print("\nel valor de y evaluada en 3 es\ny=", y)
+
