c354460cf495afe1664f8eed1f48eccd4008799d
lucianogmzz <A22310214@live.ceti.mx>
2025-03-15 11:30:23 -0600

    Practica8_if_else_elif_V1

--- 008_if_else_elif.py
+++ 008_if_else_elif.py
@@ -3,11 +3,11 @@ año = int(input("ingrese el año: "))
 if año < 1582:
     print("no esta dentro del periodo gregoriano")
 else:
-    if año % 4 != 0:
+    if año % 4 != 0:  #si el año no es divisible entre 4, es año comun
         print("año comun")
-    elif año %100 != 0:
+    elif año %100 != 0:  #si el año es divisible entre 4, pero no entre 100 es bisiesto
         print("año bisiesto")
-    elif año %400 !=0:
+    elif año %400 !=0: #si el año es divisible entre 100, pero no entre 400 es comun
         print("año comun")
-    else:
+    else:        # si es divisible entre 400, es bisiesto 
         print("año bisiesto")
