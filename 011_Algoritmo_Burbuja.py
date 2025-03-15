cc4dd27f81e025230e8cac397cfde3e3c82a0ac9
lucianogmzz <A22310214@live.ceti.mx>
2025-03-15 13:33:01 -0600

    Practica11 V1

--- 011_Algoritmo_Burbuja.py
+++ 011_Algoritmo_Burbuja.py
@@ -1,5 +1,4 @@
 list = []
-swapped = True
 
 num = int(input("cuantos numeros quieres ordenar?\n "))
 
@@ -7,11 +6,5 @@ for i in range(num):
     val = float(input("ingresa el valor a la lista: "))
     list.append(val)
     
-while swapped:
-    swapped = False 
-    for i in range(len(list) - 1):
-        if list[i] > list[i + 1]:
-            swapped = True
-            list[i], list[i + 1] = list[i + 1], list[i]
-        
-print("lista ordenada:\n ", list)
+list.sort()
+print(list)
