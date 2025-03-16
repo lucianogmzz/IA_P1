e8ffb639f18ed870622ba519dfc8e040ba6612ec
lucianogmzz <A22310214@live.ceti.mx>
2025-03-16 11:50:44 -0600

    Practica14_V1

--- 014_Funciones.py
+++ 014_Funciones.py
@@ -1,6 +1,6 @@
-def message():
-    print("Ingresar valor: ")
- 
-print("Inicia aqui.")
-message()
-print("Termina aqui.")
+def hello (name): # se define la funcion hello que tiene como argumento name
+    print("hello, ", name)
+
+name = input("ingresa tu nombre: ") #se declara el argumento de la funcion
+
+hello(name)   #se invoca la funcion
