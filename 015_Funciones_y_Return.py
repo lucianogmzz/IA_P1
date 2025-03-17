79bb5394136290320488bc5877a4a4798156d687
lucianogmzz <A22310214@live.ceti.mx>
2025-03-16 19:11:35 -0600

    Practica15

--- 015_Funciones_y_Return.py
+++ 015_Funciones_y_Return.py
@@ -1,10 +1,10 @@
-def primo(num):
+def primo(num): #se define la funcion primo
     for i in range(2, int(1 + num ** 0.5)):
-        if num % 1 == 0:
-            return  False
-    return True
+        if num % 1 == 0:   # se compueba si es primo
+            return  False  # si no lo es retona falso
+    return True  # si lo es retorna verdadero
 
-for i in range(1, 20):
+for i in range(1, 20):   # arroja los numeros primos del 1-20
     if primo(i + 1):
         print(i + 1, end= " ")
 print()
